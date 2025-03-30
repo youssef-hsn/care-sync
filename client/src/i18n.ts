98851724793a36ea5d29329eac69d1e8d7cb9f19
youssef <youssefhsn16@gmail.com>
2025-03-30 18:26:25 +0300

    📜 configure i18n internationalization support
    The purpose of the internationalization is for the dual language nature of our application

---
+++ client/src/i18n.ts
@@ -0,0 +1,27 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import HttpBackend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n
+  .use(HttpBackend)
+  .use(LanguageDetector) 
+  .use(initReactI18next) 
+  .init({
+    fallbackLng: 'ar', 
+    ns: ['common'],
+    defaultNS: 'common',
+    supportedLngs: ['ar', 'en'],
+    debug: false,
+    interpolation: {
+      escapeValue: false,
+    },
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    },
+    react: {
+      useSuspense: true,
+    },
+  });
+
+export default i18n;
