9f5e28f16cd2f2d950b295d91fc5754b2eb11f7c
youssef <youssefhsn16@gmail.com>
2025-04-04 10:26:35 +0300

    🗑️ remove terms of service and privacy policy disclaimer from LoginForm component

--- client/src/components/login-form.tsx
+++ client/src/components/login-form.tsx
@@ -63,10 +63,6 @@ export function LoginForm({
           </div>
         </CardContent>
       </Card>
-      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
-        and <a href="#">Privacy Policy</a>.
-      </div>
     </div>
   )
 }
