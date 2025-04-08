f7f9f601c0fda8b8ad5e4bc57536be95458d3a78
youssef <youssefhsn16@gmail.com>
2025-04-08 11:04:07 +0300

    ✨ add FormInput and FormPassword components

---
+++ client/src/components/molecules/form-fields.tsx
@@ -0,0 +1,47 @@
+import { FC } from "react";
+import { Input } from "@/components/atoms/input";
+import {
+FormControl,
+FormItem,
+FormLabel,
+FormMessage,
+} from "@/components/atoms/form";
+
+interface FormInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+label: string;
+placeholder?: string;
+type?: string;
+}
+
+export const FormInput: FC<FormInputProps> = ({
+    label,
+    placeholder,
+    type = "text",
+    ...inputProps
+}) => {
+    return (
+        <FormItem>
+            <FormLabel>{label}</FormLabel>
+            <FormControl>
+                <Input type={type} placeholder={placeholder} {...inputProps} />
+            </FormControl>
+            <FormMessage />
+        </FormItem>
+    )
+};
+
+export const FormPassword: FC<FormInputProps> = ({
+    label,
+    placeholder,
+    ...inputProps
+}) => {
+    return (
+        <FormItem>
+            <FormLabel>{label}</FormLabel>
+            <FormControl>
+                <Input type="password" placeholder={placeholder} {...inputProps} />
+            </FormControl>
+            <FormMessage />
+        </FormItem>
+    )
+};
