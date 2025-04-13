a6cd6516a147cd66c72b22c503a56374247dbacd
youssef <youssefhsn16@gmail.com>
2025-04-13 20:32:25 +0300

    🪝 Add schema file to export database tables for identity, clients, appointments, finance, and facilities

---
+++ server/src/db/schema.ts
@@ -0,0 +1,5 @@
+export * from './tables/identity';
+export * from './tables/clients';
+export * from './tables/appointments';
+export * from './tables/finance';
+export * from './tables/facilities';
