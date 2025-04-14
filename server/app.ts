b84a137e6b3890a58a6fc0e8d4c1786d0be36613
youssef <youssefhsn16@gmail.com>
2025-04-14 21:12:44 +0300

    🪝 Remove status endpoint and integrate API routes

--- server/app.ts
+++ server/app.ts
@@ -1,17 +1,12 @@
 import express, { Application, Request, Response } from 'express';
+import apiRoutes from '@/routes/api.routes';
 
 const app: Application = express();
 const PORT: number | string = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/status', (req: Request, res: Response) => {
-  res.json({ 
-    status: 'ok',
-    message: "Server is up",
-    timestamp: new Date().toISOString() 
-    });
-});
+app.use('/api', apiRoutes);
 
 const server = app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
