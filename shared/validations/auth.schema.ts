1b04d2374a59330edb1890860f0ec97e58dcefba
youssef <youssefhsn16@gmail.com>
2025-04-18 19:09:20 +0300

    ✨ Update registerSchema to enforce phone number as a string and improve validation messages

--- shared/validations/auth.schema.ts
+++ shared/validations/auth.schema.ts
@@ -2,8 +2,15 @@ import * as z from "zod";
 
 export const registerSchema = z
   .object({
-    fullName: z.string().min(2, { message: "Full Name is required" }),
-    phone: z.number(),
+    fullName: z.string()
+      .min(2, { message: "Full Name is required" })
+      .refine((val) => {
+        return val.split(" ").length >= 2;
+      }, { message: "Full Name must contain at least two words" }),
+    phone: z.string().min(8, "Phone number must be atleast 8 numbers  ").refine((val) => {
+      const phoneRegex = /^\+?[1-9]\d{1,14}$/;
+      return phoneRegex.test(val);
+    }, { message: "Phone number must be valid" }),
     password: z
       .string()
       .min(8, { message: "Password must be at least 8 characters" })
