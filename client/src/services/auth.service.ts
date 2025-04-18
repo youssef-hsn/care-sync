5976f8c91a7059ccbd2122500066ff7c1d3c3013
youssef <youssefhsn16@gmail.com>
2025-04-18 18:33:59 +0300

    ✨ Refactor auth service to improve type definitions and ensure withCredentials for API calls

--- client/src/services/auth.service.ts
+++ client/src/services/auth.service.ts
@@ -5,20 +5,52 @@ interface LoginCredentials {
   password: string;
 }
 
-interface LoginResponse {
+export interface LoginResponse {
   accessToken: string;
-  refreshToken: string;
   user: {
-    id: string;
-    name: string;
-    phone: string;
+    fullName: string;
     roles: Set<string>;
   };
 }
 
+interface RegisterCredentials extends LoginCredentials {
+    fullName: string;
+}
+
+export interface RegsiterResponse {
+    message: string;
+    userData: {
+        id: number;
+        phone: string;
+        firstName: string;
+        lastName: string;
+    }
+}
+
 export const authService = {
   login: async (credentials: LoginCredentials) => {
-    const { data } = await api.post<LoginResponse>('/auth/login', credentials);
+    const { data } = await api.post<LoginResponse>('/auth/login', credentials, { withCredentials: true });
     return data;
   },
+
+  register: async (credentials: RegisterCredentials) => {
+    const { data } = await api.post<RegsiterResponse>('auth/register', credentials);
+    return data
+  },
+
+  refreshToken: async () => {
+    const { data } = await api.post<{ accessToken: string }>('/auth/refresh', {}, { withCredentials: true });
+    return data;
+  },
+
+  getMe: async () => {
+    const { data } = await api.get('/auth/me');
+    return data;
+  },
+
+  logout: async () => {
+    const { data } = await api.post('/auth/logout');
+    return data;
+  }
+
 };
