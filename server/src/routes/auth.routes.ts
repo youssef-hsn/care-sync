4e1ba6fd669c1054559f86154a4f33ae157f63d8
youssef <youssefhsn16@gmail.com>
2025-04-18 09:44:45 +0300

    ✨ Update auth routes to use cookie parser for login and refresh token endpoints

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -2,13 +2,14 @@ import { Router } from 'express';
 import { getMe, login, logout, refreshToken } from '@/controllers/auth/identity.controller';
 import { register } from '@/controllers/auth/register.controller';
 import { requireAuth } from '@/middleware/auth';
+import cookieParser from 'cookie-parser';
 
 const router: Router = Router();
 
 router.post('/register', register);
-router.post('/login', login);
-router.post('/logout', requireAuth, logout);
+router.post('/login', cookieParser(), login);
+router.post('/logout', logout);
 router.get('/me', requireAuth, getMe);
-router.post('/refresh', refreshToken);
+router.post('/refresh', cookieParser(), refreshToken);
 
 export default router;
