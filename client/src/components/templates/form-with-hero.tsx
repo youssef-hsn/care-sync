470a7e6f7afa8c3e46b866f49b5d04a3b8f24d91
youssef <youssefhsn16@gmail.com>
2025-04-18 18:38:16 +0300

    ✨ Update FormWithHero to extend form props and improve flexibility

--- client/src/components/templates/form-with-hero.tsx
+++ client/src/components/templates/form-with-hero.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Card, CardContent } from '../atoms/card';
 
-interface FormWithHeroProps extends React.ComponentProps<'div'> {
+interface FormWithHeroProps extends React.ComponentProps<'form'> {
     heroImage: string;
     children?: React.ReactNode;
 }
 
-const FormWithHero: React.FC<FormWithHeroProps> = ({ heroImage, children }) => {
+const FormWithHero: React.FC<FormWithHeroProps> = ({ heroImage, children, ...formProps }) => {
     return (
         <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
@@ -17,7 +17,7 @@ const FormWithHero: React.FC<FormWithHeroProps> = ({ heroImage, children }) => {
             />
           </div>
             
-          <form className="p-6 md:p-8">
+          <form className="p-6 md:p-8" {...formProps}>
             <div className="flex flex-col gap-6">
                 {children}
             </div>
