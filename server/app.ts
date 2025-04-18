4d1c7865e1e9fb057b216e4c4b4496773f20a655
youssef <youssefhsn16@gmail.com>
2025-04-18 11:43:12 +0300

    ✨ Add CORS middleware with configurable allowed origins

--- server/app.ts
+++ server/app.ts
@@ -2,10 +2,21 @@ import 'module-alias/register';
 import 'dotenv/config';
 import express, { Application } from 'express';
 import apiRoutes from '@/routes/api.routes';
+import cors from 'cors';
 
 const app: Application = express();
 const PORT: number | string = process.env.PORT || 3000;
+const allowedOrigin = process.env.CORS_ORIGIN || "http://localhost:5173";
 
+app.use(
+  cors({
+    origin: allowedOrigin,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Authorization'],
+    credentials: true,
+  }),
+)
 app.use(express.json());
 
 app.use('/api', apiRoutes);
