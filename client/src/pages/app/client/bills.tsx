cdb8eac748f0f607bb3a5230dcd8003ccf43814f
youssef <youssefhsn16@gmail.com>
2025-05-04 20:59:43 +0300

    ✨ Refactor bill fetching and display
    Simplifies bill fetching by removing unnecessary authentication dependency.
    Also reorders columns in the data table for better readability,
    placing status as the last column.

--- client/src/pages/app/client/bills.tsx
+++ client/src/pages/app/client/bills.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/atoms/card';
 import { useTranslation } from 'react-i18next';
-import { useAuth } from '@/lib/hooks/use-auth';
 import { getMyBills } from '@/services/finance/bill.service';
 import { DataTable } from '@/components/molecules/data-table';
 
@@ -16,13 +15,11 @@ interface Bill {
 }
 const BillsPage: React.FC = () => {
   const { t } = useTranslation("finance");
-  const { accessToken } = useAuth();
 
   const { data, isLoading, error } = useQuery({
     queryKey: ['bills'],
     queryFn: async () => {
-      console.log('Fetching bills');
-      return await getMyBills({ page: 0, size: 10 }, accessToken!)},
+      return await getMyBills({ page: 0, size: 10 })},
     refetchOnWindowFocus: true,
   });
 
@@ -34,14 +31,13 @@ const BillsPage: React.FC = () => {
       <CardContent>
         <DataTable<Bill> data={data} columns={[
           { label: 'billID' },
-          { label: 'status' },
           { label: 'createdAt' },
           { label: 'updatedAt' },
           { label: 'total' },
+          { label: 'status' },
         ]}
           isLoading={isLoading}
-        >
-        </DataTable>
+        />
       </CardContent>
     </Card>
   );
