887cfee93b3a106d77fa9e28ab9fb9ac838867a4
youssef <youssefhsn16@gmail.com>
2025-05-04 21:01:29 +0300

    ✨ Refactors client bills page to use data table
    Refactors the client bills page to utilize the data table component for improved data presentation and handling.
    
    This change simplifies the component's structure by replacing the custom table implementation with a reusable data table, enhancing maintainability and consistency across the application.

--- client/src/pages/app/finance/accounts.tsx
+++ client/src/pages/app/finance/accounts.tsx
@@ -1,20 +1,10 @@
 import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/atoms/card';
-import { Skeleton } from '@/components/atoms/skeleton';
 import { useTranslation } from 'react-i18next';
-import { useAuth } from '@/lib/hooks/use-auth';
 import { getBills } from '@/services/finance/bill.service';
+import { DataTable } from '@/components/molecules/data-table';
 
-import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHeader,
-    TableHead,
-    TableRow,
-} from '@/components/atoms/table';
 
 interface Bill {
     billID: number;
@@ -24,50 +14,32 @@ interface Bill {
     createdAt: string;
     updatedAt: string;
 }
-const BillsPage: React.FC = () => {
+const ClientBillsPage: React.FC = () => {
     const { t } = useTranslation("finance");
-    const { accessToken } = useAuth();
 
-    const { data: bills, isLoading } = useQuery({
-        queryKey: ['bills'],
-        queryFn: async () => await getBills({ page: 0, size: 10 }, accessToken!)
+    const { data: clientBills, isLoading } = useQuery({
+        queryKey: ['clientBills'],
+        queryFn: async () => await getBills({ page: 0, size: 10 })
     });
 
     return (
         <Card className='m-1'>
             <CardHeader>
-                <CardTitle>{t('bills.title')}</CardTitle>
+                <CardTitle>{t('clientBills.title')}</CardTitle>
             </CardHeader>
             <CardContent>
-                {isLoading ? (
-                    <Skeleton className="h-4 w-1/2" />
-                ) : (
-                    <Table>
-                        <TableCaption>{t('bills.tableCaption')}</TableCaption>
-                        <TableHeader>
-                            <TableRow>
-                                <TableHead>{t('bills.billID')}</TableHead>
-                                <TableHead>{t('bills.clientId')}</TableHead>
-                                <TableHead>{t('bills.total')}</TableHead>
-                                <TableHead>{t('bills.status')}</TableHead>
-                                <TableHead>{t('bills.createdAt')}</TableHead>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                            {bills?.map((bill: Bill) => (
-                                <TableRow key={bill.billID}>
-                                    <TableCell>{bill.billID}</TableCell>
-                                    <TableCell>{bill.clientId}</TableCell>
-                                    <TableCell>{bill.total}</TableCell>
-                                    <TableCell>{bill.status}</TableCell>
-                                    <TableCell>{new Date(bill.createdAt).toLocaleDateString()}</TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                )}
+                <DataTable<Bill> data={clientBills} columns={[
+                    { label: 'billID' },
+                    { label: 'clientId' },
+                    { label: 'createdAt' },
+                    { label: 'updatedAt' },
+                    { label: 'total' },
+                    { label: 'status' },
+                ]}
+                isLoading={isLoading}
+                />
             </CardContent>
         </Card>
     );
 }
-export default BillsPage;
+export default ClientBillsPage;
