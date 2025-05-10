c81781f55fe5739ce0e150c78233de250caeebe2
youssef-hsn <youssefhsn16@gmail.com>
2025-05-10 21:06:05 +0300

    ✨ Rename app-pages to pages/views
    This is built on the new idea of categorizing pages into views, inspections and activities

--- client/src/lib/constants/pages/views.ts
+++ client/src/lib/constants/pages/views.ts
@@ -1,8 +1,9 @@
 import DashboardPage from "@/pages/app/dashboard";
 import WorkInProgress from "@/pages/state/work-in-progress";
 import BillsPage from "@/pages/app/client/bills";
-import ClientBillsPage from "@/pages/app/finance/accounts";
+import ClientBillsPage from "@/pages/app/finance/client-bills";
 import { Home, Calendar, HandCoins, ScrollText, User, GitCompareIcon } from "lucide-react"
+import ClientsPage from "@/pages/app/client/all";
 
 export type Page = {
   title: string;
@@ -12,42 +13,52 @@ export type Page = {
   requiredRoles?: string[];
 }
 
+export const DASHBOARD_BASE_URI = "/dashboard";
+
+export const SCHEDULE_BASE_URI = "/schedule";
+
+export const BILLS_BASE_URI = "/bills";
+
+export const CLIENTS_BASE_URI = "/clients";
+
+export const MACHINES_BASE_URI = "/machines";
+
 export const pages: Page[] = [
     {
       title: "dashboard",
-      url: "/dashboard",
+      url: DASHBOARD_BASE_URI,
       icon: Home,
       page: DashboardPage,
     },
     {
       title: "schedule",
-      url: "/schedule",
+      url: SCHEDULE_BASE_URI,
       icon: Calendar,
       page: WorkInProgress,
     },
     {
       title: "bills",
-      url: "/bills",
+      url: BILLS_BASE_URI,
       icon: ScrollText,
       page: BillsPage,
     },
     {
       title: "clients",
-      url: "/clients",
+      url: CLIENTS_BASE_URI,
       icon: User,
       page: ClientsPage,
       requiredRoles: ["associate"],
     },
     {
       title: "clientBills",
-      url: "/clients/bills",
+      url: CLIENTS_BASE_URI + "/bills",
       icon: HandCoins,
       page: ClientBillsPage,
       requiredRoles: ["associate"],
     },
     {
       title: "machines",
-      url: "/machines",
+      url: MACHINES_BASE_URI,
       icon: GitCompareIcon,
       page: WorkInProgress,
       requiredRoles: ["associate"],
