461ac135235ccbdea5db9a699c44ba2f63b9ecb4
youssef-hsn <youssefhsn16@gmail.com>
2025-05-11 22:11:01 +0300

    ✨ Refactor ClientBillsPage to remove Dialog components and streamline UI
    This commit simplifies the `ClientBillsPage` by removing the Dialog components and directly integrating the create bill button within the Card header. This change enhances the user interface by making the create bill action more accessible and improves overall layout clarity.

--- client/src/pages/app/finance/client-bills.tsx
+++ client/src/pages/app/finance/client-bills.tsx
@@ -6,14 +6,6 @@ import { getBills } from '@/services/finance/bill.service';
 import { DataTable } from '@/components/molecules/data-table';
 import { Button } from '@/components/atoms/button';
 import { PlusIcon } from 'lucide-react';
-import {
-    Dialog,
-    DialogContent,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-  } from "@/components/atoms/dialog"
-import { BillForm } from '@/components/organisms/forms/bill';
 import { BILLS_BASE_URI } from '@/lib/constants/pages/views';
 import { useNavigate } from 'react-router-dom';
 import { Bill } from 'caresync/types/bill';
@@ -27,40 +19,30 @@ const ClientBillsPage: React.FC = () => {
     });
 
     return (
-        <Dialog>
-            <Card className='m-1'>
-                <CardHeader className='flex flex-row justify-between items-center'>
-                    <CardTitle>{t('bills.clientBills')}</CardTitle>
-                    <DialogTrigger>
-                        <Button size='sm'>
-                            <PlusIcon />
-                            {t('bills.createBill')}
-                        </Button>
-                    </DialogTrigger>
-                </CardHeader>
-                <CardContent>
-                    <DataTable<Bill> data={clientBills} columns={[
-                        { label: 'billID' },
-                        { label: 'clientId' },
-                        { label: 'createdAt' },
-                        { label: 'updatedAt' },
-                        { label: 'total' },
-                        { label: 'status' },
-                    ]}
-                    isLoading={isLoading}
-                    onRowClick={(row) => {
-                        navigate(BILLS_BASE_URI + "/" + row.billID);
-                    }}
-                    />
-                </CardContent>
-            </Card>
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle>{t('bills.createBill')}</DialogTitle>
-                </DialogHeader>
-                <BillForm />
-            </DialogContent>
-        </Dialog>
+        <Card className='m-1'>
+            <CardHeader className='flex flex-row justify-between items-center'>
+                <CardTitle>{t('bills.clientBills')}</CardTitle>
+                <Button size='sm'>
+                    <PlusIcon />
+                    {t('bills.createBill')}
+                </Button>
+            </CardHeader>
+            <CardContent>
+                <DataTable<Bill> data={clientBills} columns={[
+                    { label: 'billID' },
+                    { label: 'clientId' },
+                    { label: 'createdAt' },
+                    { label: 'updatedAt' },
+                    { label: 'total' },
+                    { label: 'status' },
+                ]}
+                isLoading={isLoading}
+                onRowClick={(row) => {
+                    navigate(BILLS_BASE_URI + "/" + row.billID);
+                }}
+                />
+            </CardContent>
+        </Card>
     );
 }
 export default ClientBillsPage;
