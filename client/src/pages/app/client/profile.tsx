0cb7db3333d55d3298108ea7bc8bfc3c1f8a9c03
youssef-hsn <youssefhsn16@gmail.com>
2025-05-21 08:27:48 +0300

    ✨ Refactor ClientProfilePage to include detailed sections for prescriptions, records, allergies, reports, appointments, and bills
    This commit enhances the ClientProfilePage by integrating various profile sections, such as Prescriptions, Records, Allergies, Reports, Appointments, and Bills, improving the layout and user experience. The new components are added to provide a comprehensive overview of client information, ensuring better organization and accessibility.

--- client/src/pages/app/client/profile.tsx
+++ client/src/pages/app/client/profile.tsx
@@ -1,9 +1,14 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/atoms/card";
 import LoadingPage from "@/pages/state/loading";
 import { getClient } from "@/services/client.service";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import { ClientCard } from "./components/client-card";
+import { Prescriptions } from "./components/profile-sections/prescriptions";
+import { Bills } from "./components/profile-sections/bills";
+import { Records } from "./components/profile-sections/records";
+import { Appointments } from "./components/profile-sections/appointments";
+import { Reports } from "./components/profile-sections/reports";
+import { AllergiesCard } from "./components/profile-sections/allergies";
 
 const ClientProfilePage = () => {
     const { id } = useParams();
@@ -22,26 +27,14 @@ const ClientProfilePage = () => {
     }
 
     return (
-        <div className="m-1 flex flex-wrap gap-5">
+        <div className="m-1 flex flex-wrap gap-5 justify-center md:justify-start">
             <ClientCard client={client} noActions />
-            <Card className="w-full md:w-2/3">
-                {/* Prescriptions */}
-            </Card>
-            <Card className="w-full md:w-5/6">
-                {/* Bills */}
-            </Card>
-            <Card className="w-full md:w-1/4">
-                {/* Notes */}
-            </Card>
-            <Card className="w-full md:w-1/3">
-                {/* Appointments */}
-            </Card>
-            <Card className="w-full md:w-1/4">
-                {/* Documents */}
-            </Card>
-            <Card className="w-full md:w-1/4">
-                {/* Allergies */}
-            </Card>
+            <Prescriptions clientID={Number(id)} className="w-full md:w-2/3" />
+            <Records clientID={Number(id)} className="w-full md:w-1/2" />
+            <AllergiesCard clientID={Number(id)} className="w-full md:w-48/100" />
+            <Reports clientID={Number(id)} className="w-full md:w-48/100" />
+            <Appointments clientID={Number(id)} className="w-full md:w-1/2" />
+            <Bills clientID={Number(id)} className="w-full" />
         </div>
     )
 }
