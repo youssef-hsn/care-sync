67c92e6019d245449ce4d22a464edba7f719e7a2
youssef-hsn <youssefhsn16@gmail.com>
2025-05-23 21:04:20 +0300

    ✨ Introduce BillStatus type and update Bill type structure
    This commit adds a new BillStatus type to define possible bill statuses (pending, paid, cancelled) and modifies the Bill type to replace clientID with appointmentID. These changes enhance type safety and improve the clarity of the bill data structure.

--- shared/types/bill.ts
+++ shared/types/bill.ts
@@ -1,16 +1,18 @@
+export type BillStatus = "pending" | "paid" | "cancelled";
+
 export type BillDetail = {
     service: string;
     description: string;
     reason: string;
     amount: number;
 }
-
 export type Bill = {
     billID: number;
-    clientID?: number;
+    clientID: number;
+    appointmentID?: number;
     createdAt: Date;
     updatedAt: Date;
     total: number;
-    status: string;
+    status: BillStatus;
     services?: BillDetail[];
 }
